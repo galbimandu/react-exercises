f125ec6408cc269c67f37997e5dd4e53fe8331f2
junyeongCHOI <asxd153@gmail.com>
2020-04-13 22:20:13 +0900

    finish

--- src/components/SearchBox/SearchBox.js
+++ src/components/SearchBox/SearchBox.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./SearchBox.css";
 
 class SearchBox extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <input
